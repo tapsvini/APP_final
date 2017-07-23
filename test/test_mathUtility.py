8af055f33309e052a5499706526c4b8c602dd58c
Varun <Varun@Varun.local>
2017-07-23 00:58:43 -0400

    Test Cases for testing MathUtility class Functionality

--- test/test_mathUtility.py
+++ test/test_mathUtility.py
@@ -3,25 +3,59 @@ from src.util.MathUtil import MathUtility
 
 class TestMathUtility(TestCase):
     def test_factorial(self):
-        self.fail()
+        """Test case to check weather factorial of number is accurate.
+
+        :return:Pass if factorial of value is accurate.
+        """
+        num=5
+        self.assertEqual(MathUtility.factorial(5),120)
 
     def test_roundno(self):
-        self.fail()
 
-    def test_chudnovsky(self):
-        self.fail()
+        """Test case to check weather rounding off number is accurate.
+
+        :return:Pass if rounding of value is accurate.
+        """
+
+        num=3.1432
+        self.assertEqual(MathUtility.roundno(num,2),3.14)
 
     def test_chudnovsky_big(self):
-        self.fail()
+
+        """Test case to check weather pi value is cacluting right.
+
+        :return:Pass if pi value is accurate.
+        """
+
+        self.assertEqual(int(MathUtility.chudnovsky_big(30)), int(3.14))
 
     def test_find_sin(self):
-        self.fail()
+        """Test case to check value of sin
+
+        :return:Pass if value of sin is accurate.
+        """
+        self.assertEqual(int(MathUtility.find_sin(22, 2)), int(0))
 
     def test_find_cos(self):
-        self.fail()
+        """Test case to check value of cos
+
+        :return:Pass if valueo of cos is accurate.
+        """
+        self.assertEqual(MathUtility.find_cos(30,22,2),0.86)
 
     def test_abs_err(self):
-        self.fail()
+        """Test case to check absolute error working fine
+
+        :return:Pass if absolute error is accurate.
+        """
+        self.assertEqual(MathUtility.abs_err(2,1),1)
 
     def test_rel_err(self):
-        self.fail()
+
+        """Test case to check relative error working fine
+
+        :return:Pass if relative error is accurate.
+        """
+
+        self.assertEqual(MathUtility.rel_err(2, 1), 0.5)
+
